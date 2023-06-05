a95f9e1df0273630217aa16c746d6002f3aaf598
tosecformback <sub7503@naver.com>
2023-06-05 23:38:53 +0900

    모듈화 준비

---
+++ resource/search_and_result.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("page_body")
+const section = document.createElement("section");
+const article = document.createElement("article");
+
+function search_area(){
+    const input = document.createElement("input");
+    let kwd_input = input.create
+}
+function result_area(){
+
+}
